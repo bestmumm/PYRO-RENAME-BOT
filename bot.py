6537d19b0d6c5e016433b6aab0eef9832fa09ce3
Znabu <bestmunum@gmail.com>
2024-08-02 02:03:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,8 +35,10 @@ class Bot(Client):
                     await web.TCPSite(app, "0.0.0.0", 8080).start()     
                 print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
                 for id in Config.ADMIN:
-                    try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
-                    except: pass
+                    try: 
+                        await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
+                    except: 
+                        pass
                 if Config.LOG_CHANNEL:
                     try:
                         curr = datetime.now(timezone("Asia/Kolkata"))
